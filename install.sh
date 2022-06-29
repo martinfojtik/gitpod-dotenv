0279c9bbf1853fe1078746d74c959b9c0a960a44
Martin Fojtík <m.fojtik@seznam.cz>
2022-06-29 15:00:41 +0200

    copy aliases

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 echo Running custom install scritp
 
-npm install -g npm-check-updates sort-package-json
+npm install -g npm-check-updates sort-package-json npm@latest
 
+cp .bash_aliases /home/gitpod/.bash_aliases
