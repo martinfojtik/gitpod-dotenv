a0a4d7dbcb16a6d16e54c7180137d53806f90606
Martin Fojtík <m.fojtik@seznam.cz>
2022-06-29 15:09:14 +0200

    fix copy aliases

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
-echo Running custom install scritp
-
 npm install -g npm-check-updates sort-package-json npm@latest
 
-cp .bash_aliases /home/gitpod/.bash_aliases
+cp /home/gitpod/.dotfiles/.bash_aliases /home/gitpod/.bash_aliases
